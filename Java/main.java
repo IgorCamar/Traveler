d49600131624531362ebf17add14d404144ef5ab
igor camara <igorcamaras@gmail.com>
2022-09-06 00:53:00 -0300

    update

--- Java/main.java
+++ Java/main.java
@@ -1,15 +1,12 @@
 import java.util.Date;
 
-
 public class main {
 
 	public static void main(String[] args) {
-		
-		
-	ClienteDAO  ClienteDAO = new ClienteDAO();
-	
-	
-		//inserindo dados Cliente--------------------
+
+		ClienteDAO ClienteDAO = new ClienteDAO();
+
+		// inserindo dados Cliente--------------------
 		Cliente Cliente = new Cliente();
 		//
 		Cliente.setNome("null");
@@ -18,38 +15,52 @@ public class main {
 		Cliente.setFone(0);
 		//
 		ClienteDAO.save(Cliente);
-		//deletando
+		// deletando
 		ClienteDAO.removeByCPF(null);
-		
-		//inserindo dados Servico-------------------
-	ServicoDAO  ServicoDAO = new ServicoDAO();
-	
-		Servico Servico = new Servico();
-		//
-		Servico.setValor(0);
-		Servico.setTipoServico("");
-		Servico.setDescricao("");
-		//
-		ServicoDAO.save(Servico);
-		//deletando
-		ServicoDAO.removeById(0);
-		
-		//inserindo dados pedido----------------------
-	PedidoDAO PedidoDAO = new PedidoDAO();
-	
-		//
-		Pedido Pedido = new Pedido();
-		//
-		Pedido.setData(new Date());
-		
-		//
-		PedidoDAO.save(Pedido);
-		//deletando------------------------------
-		PedidoDAO.removeById(0);
-		//
-		
-		
-		
+		// Visualização dos registros do banco de dados TODOS
+		for (Cliente c : ClienteDAO.getClientes()) {
+			System.out.println("Cliente: " + c.getCpf());
+			System.out.println("Nome: " + c.getNome());
+			System.out.println("----------------------------");
+			}
+			// inserindo dados Servico-------------------
+			ServicoDAO ServicoDAO = new ServicoDAO();
+
+			Servico Servico = new Servico();
+			//
+			Servico.setValor(0);
+			Servico.setTipoServico("");
+			Servico.setDescricao("");
+			//
+			ServicoDAO.save(Servico);
+			// deletando
+			ServicoDAO.removeById(0);
+			// Visualização dos registros do banco de dados TODOS
+			for (Servico s : ServicoDAO.getServicos()) {
+				System.out.println("ID: " + s.getIdServico());
+				System.out.println("ID: " + s.getValor());
+				System.out.println("----------------------------");
+				}
+			// inserindo dados pedido----------------------
+			PedidoDAO PedidoDAO = new PedidoDAO();
+
+			//
+			Pedido Pedido = new Pedido();
+			//
+			Pedido.setData(new Date());
+
+			//
+			PedidoDAO.save(Pedido);
+			// deletando------------------------------
+			PedidoDAO.removeById(0);
+			// Visualização dos registros do banco de dados TODOS
+			for (Pedido p : PedidoDAO.getPedidos()) {
+				System.out.println("ID: " + p.getNumPedido());
+				System.out.println("ID: " + p.getInfoPedido());
+				System.out.println("----------------------------");
+				}
+
+		}
+
 	}
 
-}
